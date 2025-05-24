d5a6fde0d70544967144871353396033ae3c596b
TinHtunOo <tinhtunhe@gmail.com>
2025-05-25 01:25:19 +0700

    add signup features

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 
 const StyledFormRow = styled.div`
   display: flex;
-  gap: 2px;
-  flex-direction: column;
-  padding: 15px 0;
+  gap: 10px;
+  padding: 10px 0;
+  justify-content: end;
+  align-items: center;
 
   color: var(--color-grey-600);
   font-size: 1.6rem;
@@ -14,17 +15,25 @@ const StyledFormRow = styled.div`
 
 const StyledLabel = styled.span`
   font-size: 1.5rem;
+  min-width: 300px;
+`;
+
+const StyledError = styled.div`
+  color: var(--color-red-700);
+  min-width: 300px;
 `;
 FormRowVertical.propTypes = {
   label: PropTypes.string,
   children: PropTypes.node,
+  error: PropTypes.string,
 };
 
-function FormRowVertical({ label, children }) {
+function FormRowVertical({ label, children, error }) {
   return (
     <StyledFormRow>
       <StyledLabel>{label}</StyledLabel>
       {children}
+      <StyledError>{error ? error : ""}</StyledError>
     </StyledFormRow>
   );
 }
